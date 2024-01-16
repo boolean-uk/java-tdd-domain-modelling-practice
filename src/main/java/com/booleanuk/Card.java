dd4e98e57a558c27041f7084204d1c83bed6763f
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-16 15:58:21 +0100

    createDeckTest passed

---
+++ src/main/java/com/booleanuk/Card.java
@@ -0,0 +1,13 @@
+package com.booleanuk;
+
+public class Card {
+    public Suit suit;
+    public int value;
+
+    public Card(Suit suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+}
+
--- src/main/java/com/booleanuk/Deck.java
+++ src/main/java/com/booleanuk/Deck.java
@@ -1,5 +1,22 @@
 package com.booleanuk;
 
-public class Deck {
+import java.util.ArrayList;
 
+public class Deck {
+    public ArrayList<Card> cards;
+
+    public Deck() {
+        create();
+    }
+
+
+    private void create() {
+        cards = new ArrayList<>();
+        for(int value = 2; value <= 14; value++) {
+            cards.add(new Card(Suit.HEARTS, value));
+            cards.add(new Card(Suit.CLUBS, value));
+            cards.add(new Card(Suit.DIAMONDS, value));
+            cards.add(new Card(Suit.SPADES, value));
+        }
+    }
 }
