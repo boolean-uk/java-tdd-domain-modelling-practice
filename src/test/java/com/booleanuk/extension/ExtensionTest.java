da19bcae8ceb8e482f6d356f43c0bae24e617197
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-16 15:47:10 +0100

    createDeckTest created

--- src/test/java/com/booleanuk/extension/ExtensionTest.java
+++ src/test/java/com/booleanuk/extension/ExtensionTest.java
@@ -1,4 +1,14 @@
 package com.booleanuk.extension;
 
+import com.booleanuk.Deck;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ExtensionTest {
+
+    @Test
+    public void () {
+        Deck deck = new Deck();
+        Assertions.assertEquals(52, deck.cards.size());
+    }
 }
