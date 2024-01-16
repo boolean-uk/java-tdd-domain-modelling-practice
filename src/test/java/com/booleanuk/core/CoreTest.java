21767fb059bbcca65e274c3f84548f0c0a97fac6
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-16 09:39:22 +0100

    Add test for menu sysout

--- src/test/java/com/booleanuk/core/CoreTest.java
+++ src/test/java/com/booleanuk/core/CoreTest.java
@@ -1,16 +1,37 @@
 package com.booleanuk.core;
 
 import com.booleanuk.Main;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class CoreTest {
 
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
 
     @Test
     public void testMenuOptions() {
         Main main = new Main();
 
         main.menu();
+
+        Assertions.assertEquals("Welcome to the menu", outputStreamCaptor.toString()
+                .trim());
 
     }
 }
