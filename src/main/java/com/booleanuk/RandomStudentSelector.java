b5d9354fc85d3099f7068d44e363f95ad40cf326
Marit <marit.moe2@ntebb.no>
2024-01-16 10:19:40 +0100

    Code to pass showMenu() test.

--- src/main/java/com/booleanuk/RandomStudentSelector.java
+++ src/main/java/com/booleanuk/RandomStudentSelector.java
@@ -2,4 +2,16 @@ package com.booleanuk;
 
 public class RandomStudentSelector {
 
+    public RandomStudentSelector() {
+
+    }
+
+    public void showMenu() {
+        String menu = "This is a random student selector.\n" +
+                "S: Get random student.\n" +
+                "P: Get random pairs.\n" +
+                "G: Get random pair groups.\n" +
+                "X: Exit program.";
+        System.out.println(menu);
+    }
 }
