0040ff0a2a2c3547b46eacb2d9107552f8124787
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-16 10:37:14 +0100

    tried adding test for scanner in while loop but have problems

--- src/main/java/com/booleanuk/Main.java
+++ src/main/java/com/booleanuk/Main.java
@@ -1,5 +1,7 @@
 package com.booleanuk;
 
+import java.util.Scanner;
+
 public class Main {
     public void menu() {
 
@@ -8,7 +10,21 @@ public class Main {
 
 
     public void program() {
+        Scanner scan = new Scanner(System.in);
+
+        String input = "";
+        while(!input.equalsIgnoreCase("q")) {
+            menu();
+            if(scan.hasNextLine()) {
+                input = scan.nextLine();
+            }
+        }
+        scan.close();
+    }
 
 
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.program();
     }
 }
--- src/test/java/com/booleanuk/core/CoreTest.java
+++ src/test/java/com/booleanuk/core/CoreTest.java
@@ -6,22 +6,37 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 
 public class CoreTest {
 
     private final PrintStream standardOut = System.out;
+    private final InputStream systemIn = System.in;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
 
     @BeforeEach
     public void setUp() {
         System.setOut(new PrintStream(outputStreamCaptor));
     }
 
+
     @AfterEach
     public void tearDown() {
         System.setOut(standardOut);
+        System.setIn(systemIn);
+    }
+
+    private void provideInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return outputStreamCaptor.toString().trim();
     }
 
     @Test
@@ -30,19 +45,20 @@ public class CoreTest {
 
         main.menu();
 
-        Assertions.assertEquals("Welcome to the menu", outputStreamCaptor.toString()
-                .trim());
+        Assertions.assertEquals("Welcome to the menu", getOutput());
 
     }
+
 
     @Test
     public void testProgramRuns() {
         Main main = new Main();
 
         main.program();
+        provideInput("q");
 
-        Assertions.assertEquals("Welcome to the menu", outputStreamCaptor.toString()
-                .trim());
+        //Assertions.assertEquals("q", getOutput());
+
 
     }
 }
