ae2181973cfcd63611b9918871b41eb8bf15a84e
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-16 09:49:31 +0100

    testShowMenu created

--- src/test/java/com/booleanuk/core/CoreTest.java
+++ src/test/java/com/booleanuk/core/CoreTest.java
@@ -1,4 +1,16 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CoreTest {
+
+    @Test
+    public void testShowMenu() {
+        RandomStudentSelector randomStudentSelector = new RandomStudentSelector();
+        String expected = "Welcome! /n/nPlease select an option./n/nA. Get a random student./nB. Get randomised pairs./nC. Get randomised groups of four.";
+        Assertions.assertEquals(expected, randomStudentSelector.showMenu());
+    }
 }
+
+
