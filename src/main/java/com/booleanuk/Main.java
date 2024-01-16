fb1848aa631b9336f45c2773435f75acb18eb011
Ateeb <ateeb@live.no>
2024-01-16 10:37:37 +0100

    Ateeb salam

--- src/main/java/com/booleanuk/Main.java
+++ src/main/java/com/booleanuk/Main.java
@@ -1,4 +1,8 @@
 package com.booleanuk;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
 }
--- src/test/java/com/booleanuk/core/CoreTest.java
+++ src/test/java/com/booleanuk/core/CoreTest.java
@@ -1,4 +1,18 @@
 package com.booleanuk.core;
 
+import com.booleanuk.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
 public class CoreTest {
+    @Test
+    public void testRandomFunction(){
+        Main main = new Main();
+        Random random = new Random(100);
+        Assertions.assertEquals(main.functions[1],main.getFunctions());
+
+
+    }
 }
