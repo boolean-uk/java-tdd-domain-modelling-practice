9b516601a9207cdd6c22d7ab3f3c5b9e8931394f
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-16 09:34:18 +0100

    Add test for menu

--- src/test/java/com/booleanuk/core/CoreTest.java
+++ src/test/java/com/booleanuk/core/CoreTest.java
@@ -1,4 +1,16 @@
 package com.booleanuk.core;
 
+import com.booleanuk.Main;
+import org.junit.jupiter.api.Test;
+
 public class CoreTest {
+
+
+    @Test
+    public void testMenuOptions() {
+        Main main = new Main();
+
+        main.menu();
+
+    }
 }
