8c00eda536740befe8ac92e87730bddad6f1404c
Marit <marit.moe2@ntebb.no>
2024-01-16 10:15:09 +0100

    Test showMenu()

--- src/test/java/com/booleanuk/RandomStudentSelectorTest.java
+++ src/test/java/com/booleanuk/RandomStudentSelectorTest.java
@@ -1,5 +1,33 @@
 package com.booleanuk;
 
-public class RandomStudentSelectorTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class RandomStudentSelectorTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    public void testShowMenuOutputsStringOfOptions() {
+        String menu = "This is a random student selector.\nS: Get random student.\nP: Get random pairs.\nG: Get random pair groups.\nX: Exit program.";
+        RandomStudentSelector studentSelector = new RandomStudentSelector();
+        studentSelector.showMenu();
+        Assertions.assertEquals(menu, outputStreamCaptor.toString()
+                .trim());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
 }
