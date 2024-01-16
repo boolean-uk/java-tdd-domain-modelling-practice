5bc9d1dcd08ad9f9f9ff54f8104eb5199677f510
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-16 09:51:22 +0100

    testShowMenu passed

---
+++ src/main/java/com/booleanuk/RandomStudentSelector.java
@@ -0,0 +1,9 @@
+package com.booleanuk;
+
+public class RandomStudentSelector {
+
+    public String showMenu() {
+        return "Welcome! /n/nPlease select an option./n/nA. Get a random student./nB. " +
+                "Get randomised pairs./nC. Get randomised groups of four.";
+    }
+}
