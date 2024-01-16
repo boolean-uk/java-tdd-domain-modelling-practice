e7cfdb342904819bfe9c0c9af3b18451e1f777ef
tonnesegeland <to.smith.eg@gmail.com>
2024-01-16 09:57:33 +0100

    1. test, failed, not something i want to test due to dependent on user input

--- src/main/java/com/booleanuk/Student.java
+++ src/main/java/com/booleanuk/Student.java
@@ -1,6 +1,31 @@
 package com.booleanuk;
 
+import java.util.Scanner;
+
 public class Student {
+    Scanner input = new Scanner(System.in);
+
+    public Student(){
+        System.out.println("s");
+    }
+
+
+    public String userMenu() {
+        String userInput = "";
+
+        // Display menu to User
+        System.out.println("Student Class Menu");
+        System.out.println("R: Random Student");
+
+        // Take user input
+        userInput = input.nextLine().toUpperCase();
+
+        if ( !userInput.equals("B") ){
+            userInput ="";
+        }
+        return userInput;
+
+    }
 
 
 }
