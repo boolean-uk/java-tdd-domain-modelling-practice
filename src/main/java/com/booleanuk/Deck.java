449e27933ae8074c124111cd857b79ae882a56c4
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-17 09:50:54 +0100

    createHandTest passed

--- src/main/java/com/booleanuk/Deck.java
+++ src/main/java/com/booleanuk/Deck.java
@@ -32,11 +32,11 @@ public class Deck {
         }
     }
 
-    private void shuffle() {
+    public void shuffle() {
         Collections.shuffle(cards, randomiser);
     }
 
     public ArrayList<Card> createHand(int noOfCards) {
-        return new ArrayList<>();
+        return new ArrayList<Card>(cards.subList(0, noOfCards));
     }
 }
